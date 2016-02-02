ec4ed7bf5d6d31e92cca994cbc7500c008023d9f
Kirsten Cassidy <mantismamita@gmail.com>
2016-02-02 22:19:22 +0000

    adds script for updating several sites on the same server

---
+++ maj-allsites.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# changer les nom et chemins des sites selon le serveur
+
+declare -a sites=('/srv/www/wp1/htdocs' '/srv/www/wp2/htdocs' )
+cd
+for site in "${sites[@]}";
+do
+wp --path=$site core update --debug
+done
